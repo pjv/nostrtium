af01d20db4228e101f9e1b47e929c76020cce16d
pjv <pjv@users.noreply.github.com>
2023-03-31 08:19:41 -0500

    update readme

--- wp-nostr.php
+++ wp-nostr.php
@@ -1,23 +1,27 @@
 <?php
 
 /**
- * Plugin Name:     WP Nostr
- * Plugin URI:      https://github.com/pjv/wp-nostr
- * Description:     Post to Nostr from WordPress
- * Author:          pjv
- * Author URI:      https://github.com/pjv
- * Text Domain:     wp-nostr
- * Domain Path:     /languages
- * Version:         0.5.2
- *
- * @package         WP_Nostr
+ * Plugin Name:      WP Nostr
+ * Plugin URI:       https://github.com/pjv/wp-nostr
+ * Description:      Post to Nostr from WordPress
+ * Author:           pjv
+ * Author URI:       https://github.com/pjv
+ * Text Domain:      wp-nostr
+ * Domain Path:      /languages
+ * Version:          0.5.3
+ * Requires at least 6.0
+ * Requires PHP      8.1
+ * License           Unlicense
+ * License URI       https://unlicense.org
+ * 
+ * @package          WP_Nostr
  */
 
 if (!defined('WPINC')) {
   exit;
 }
 
-define('PJV_WPNOSTR_VERSION', '0.5.2');
+define('PJV_WPNOSTR_VERSION', '0.5.3');
 define('PJV_WPNOSTR_DIR', plugin_dir_path(__FILE__));
 define('PJV_WPNOSTR_DEFAULT_USER_ROLE', 'edit_posts');
 

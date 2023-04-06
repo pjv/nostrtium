878de8ad3409bf3f2ed8319e100f54a37ef0af58
pjv <pjv@users.noreply.github.com>
2023-04-06 14:21:42 -0500

    oops - rookie error

--- nostrtium.php
+++ nostrtium.php
@@ -8,7 +8,7 @@
  * Author URI:       https://github.com/pjv
  * Text Domain:      nostrtium
  * Domain Path:      /languages
- * Version:          0.5.3
+ * Version:          0.5.4
  * Requires at least 6.0
  * Requires PHP      8.1
  * License           Unlicense
@@ -21,24 +21,24 @@ if (!defined('WPINC')) {
   exit;
 }
 
-define('PJV_NOSTRTIUM_VERSION', '0.5.3');
+define('PJV_NOSTRTIUM_VERSION', '0.5.4');
 define('PJV_NOSTRTIUM_DIR', plugin_dir_path(__FILE__));
 define('PJV_NOSTRTIUM_DEFAULT_USER_ROLE', 'edit_posts');
 
 require_once PJV_NOSTRTIUM_DIR . 'classes/class-nostrtium-requirements-check.php';
-$PJV_Nostrtium_requirements_check = new Nostrtium_Requirements_Check([
+$pjv_nostrtium_requirements_check = new Nostrtium_Requirements_Check([
   'title' => 'Nostrtium',
   'php'   => '8.1',
   'wp'    => '6.0',
   'dir'   => PJV_NOSTRTIUM_DIR,
   'file'  => __FILE__,
 ]);
-if ($PJV_Nostrtium_requirements_check->passes()) {
+if ($pjv_nostrtium_requirements_check->passes()) {
   require_once PJV_NOSTRTIUM_DIR . '/vendor/autoload.php';
   require_once PJV_NOSTRTIUM_DIR . 'classes/class-nostrtium.php';
-  $plugin = Nostrtium::get_instance();
+  $pjv_nostrtium_plugin = Nostrtium::get_instance();
 
-  register_activation_hook(__FILE__, [$plugin, 'activate']);
-  register_deactivation_hook(__FILE__, [$plugin, 'deactivate']);
+  register_activation_hook(__FILE__, [$pjv_nostrtium_plugin, 'activate']);
+  register_deactivation_hook(__FILE__, [$pjv_nostrtium_plugin, 'deactivate']);
 }
-unset($PJV_Nostrtium_requirements_check);
+unset($pjv_nostrtium_requirements_check);

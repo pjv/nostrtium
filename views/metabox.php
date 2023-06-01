966a1063ab572e6282462c3f6e0c27e471af8891
pjv <pjv@users.noreply.github.com>
2023-06-01 10:22:09 -0500

    delete unneeded php tag; fix whitespace

--- views/metabox.php
+++ views/metabox.php
@@ -2,15 +2,13 @@
   echo get_the_excerpt($post->ID) . "\n\n";
   echo get_permalink($post->ID);?>
 </textarea>
-<?php if($post->_nostrtium_posted): ?>
-<button disabled="disabled" id="nostrtium-post" class="ui button" style="margin-top:15px">POSTED</button>
-<?php else: ?>
-<button id="nostrtium-post" class="ui button" style="margin-top:15px">Post to Nostr</button>
+<?php if ($post->_nostrtium_posted) : ?>
+  <button disabled="disabled" id="nostrtium-post" class="ui button" style="margin-top:15px">POSTED</button>
+<?php else : ?>
+  <button id="nostrtium-post" class="ui button" style="margin-top:15px">Post to Nostr</button>
 <?php endif; ?>
 
 <div class="modal">
   <h2>Posting to Nostr</h2>
   <p class="" id="nostr-log">Please wait...</p>
 </div>
-
-<?php
